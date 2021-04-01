6e06fd0d3918173ed4df64c58f79be1dea422839
leam <leam.murphy1@gmail.com>
2021-04-01 00:19:13 +0000

    fixed with eslint

--- eqArrays.js
+++ eqArrays.js
@@ -22,8 +22,9 @@ const eqArrays = function(arr1, arr2) {
   return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3, 4]));
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+//assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
