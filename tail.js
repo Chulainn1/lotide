9f596a27cce340e531920ac3eaadc2cbef781e6f
leam <leam.murphy1@gmail.com>
2021-05-24 23:27:43 +0000

    fixed the tail function to compare indices and assertEqual

--- tail.js
+++ tail.js
@@ -7,15 +7,18 @@ const assertEqual = function(actual, expected) {
   
 };
   
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
   
 const tail = function(arr) {
   return arr.slice(1);
 };
 
 const test = ["1", "2", "3"];
+const result = tail(test);
 
-console.log(tail(test));
+//console.log(tail(test));
+
+assertEqual((result.length), 2);
+assertEqual(result[0],"2");
+assertEqual(result[1],"3");
 
 assertEqual(test.length, 3);
