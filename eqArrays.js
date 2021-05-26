e958e4ed02e5920381a7b4dc54158444d2ec6754
leam <leam.murphy1@gmail.com>
2021-05-26 02:31:51 +0000

    did some linting

--- eqArrays.js
+++ eqArrays.js
@@ -10,21 +10,21 @@ const assertEqual = function(actual, expected) {
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual(1, 1);
   
-const eqArrays = function(arr1, arr2) { 
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i =0; i < arr1.length; i++) {
+  for (let i = 0; i < arr2.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3, 4]));
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-//assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
 
-//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-//assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
